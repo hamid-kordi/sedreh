bf63a5b36988631cfacec352eb611166be2048a7
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-17 22:00:20 +0330

    add model user and book

--- sample_sedreh/book/models.py
+++ sample_sedreh/book/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.urls import reverse
+from ckeditor.fields import RichTextField
 
+# from ckeditor.fields import RichTextField
 # Create your models here.
+
+
+class Category(models.Model):
+    sub_category = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        related_name="scategory",
+        null=True,
+        blank=True,
+    )
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "my_cetegory"
+        verbose_name_plural = "my_cetegories"
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    category = models.ManyToManyField(Category, related_name="products")
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField()
+    description = RichTextField(blank=True, null=True)
+    price = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    file = models.FileField(upload_to="book_files/", blank=True, null=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
--- sample_sedreh/user/models.py
+++ sample_sedreh/user/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser, AbstractUser
+from .managers import UserManagers
+from django.utils import timezone
+from book.models import Book
 
 # Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True, blank=False, null=True)
+    budget = models.IntegerField(default=0)
+    objects = UserManagers()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    def increaseـtheـbudget(self, user_id, code):
+        pass
+
+    def budget_cuts(self, user_id, book_id):
+        pass
+
+    def return_the_book(self, user_id, book_id):
+        pass
+
+
+class OtpCode(models.Model):
+    email = models.EmailField(max_length=11, unique=True)
+    code = models.PositiveSmallIntegerField(blank=True, null=True)
+    created = models.DateTimeField(auto_now=True)
+    price = models.IntegerField(default=0, null=False, blank=True)
+    expiration_date = models.DateTimeField(default=timezone)
+
+    def __str__(self):
+        return f"{self.email} - {self.code} - {self.created}"
+
+
+class Library(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
