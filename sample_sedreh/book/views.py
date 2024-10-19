89764fc87a67b39351b80cd547343bc5f63f1084
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-19 20:45:54 +0330

    add bokk and cetegory view

--- sample_sedreh/book/views.py
+++ sample_sedreh/book/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Book
+from .serializers import CategorySerializer, BookSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class CtegoryManage(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# class BookManage(viewsets.ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
