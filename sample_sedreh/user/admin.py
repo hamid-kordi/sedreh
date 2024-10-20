78d83ccda10b0512c00973cdd76b4ef1a9aad1fd
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-20 17:22:09 +0330

    register library

--- sample_sedreh/user/admin.py
+++ sample_sedreh/user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Library
 
 
 # Register your models here.
@@ -31,3 +31,6 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+
+
+admin.site.register(Library)
