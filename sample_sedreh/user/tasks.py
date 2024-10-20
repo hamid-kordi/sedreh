12a8dccfa633bd1a9c194dc9bba5a084df1bdd52
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-20 17:22:46 +0330

    make instance

--- sample_sedreh/user/tasks.py
+++ sample_sedreh/user/tasks.py
@@ -6,14 +6,16 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 @shared_task
-def celery_increaseـtheـbudget(username, code):
+def celery_increaseـtheـbudget(user_id, code):
     try:
-        otp_code = OtpCode.objects.filter(username=username, code=code)
+        otp_code = OtpCode.objects.get(user=user_id, code=code)
         if otp_code.expiration_date < timezone.now():
             return {"status": "error", "message": "OTP code is expired."}
-        user = User.objects.filter(username=username)
+        user = otp_code.user
         user.budget += otp_code.price
         user.save()
+        otp_code.delete()
+        return {"status": "success", "message": "Budget increased successfully."}
     except OtpCode.DoesNotExist:
         return {"status": "error", "message": "Invalid OTP code."}
     except User.DoesNotExist:
@@ -22,12 +24,14 @@ def celery_increaseـtheـbudget(username, code):
         return {"status": "error", "message": str(e)}
 
 
+####
 @shared_task
 def celery_buy_book(username, book_id):
     try:
-        user = User.objects.filter(username=username)
-        book = Book.objects.get(id=book_id)
-        if Library.objects.filter(book=book, user=user):
+        user = User.objects.filter(username=username).first()
+        book = Book.objects.filter(id=book_id)
+        user_id = user.id
+        if Library.objects.filter(user=user_id, book=book_id).exists():
             return "This book is already in your library."
         if user.budget < book.price:
             return "Your budget is insufficient to buy this book."
@@ -46,8 +50,8 @@ def celery_return_book(username, book_id):
     try:
         user = User.objects.get(username=username)
         book = Book.objects.get(id=book_id)
-
-        library_entry = Library.objects.filter(user=user, book=book).first()
+        user_id = user.id
+        library_entry = Library.objects.filter(user=user_id, book=book_id).first()
         if not library_entry:
             return "This book is not in your library."
         if timezone.now() - library_entry.added < timezone.timedelta(hours=1):
